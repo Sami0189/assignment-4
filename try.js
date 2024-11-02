a098dc622ed80fc60c806931855ab0d3385e8860
Sami0189 <samisaimon28@gmail.com>
2024-11-02 15:09:32 +0600

    finish

--- try.js
+++ try.js
@@ -1,10 +1,34 @@
-function endsVowel(str){
-    for (let i of "aeiou"){
-        if (str.endsWith(i)){
-            return true;
-        }
-    }
-    return false;
+// function endsVowel(str){
+//     for (let i of "aeiou"){
+//         if (str.endsWith(i)){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+// const sa = endsVowel('sami');
+// console.log(sa);
+
+
+
+function password(obj){
+    const year = obj.birthYear;
+
+    //console.log(year.toString().length);
+
+   if(obj.birthYear.toString().length!==4){
+    return 'invalid';
+   }
+
+if(!obj.birthYear || !obj.name || !obj.siteName){
+    return 'invalid';
 }
-const sa = endsVowel('sami');
-console.log(sa);
+   
+
+   //console.log(obj);    
+
+   const man = obj.siteName+ '#' +obj.name+ '@' + obj.birthYear;
+   return man;
+}
+const object = password({name:'kolimuddin',birthYear:1999,siteName:'google'});
+console.log(object);
